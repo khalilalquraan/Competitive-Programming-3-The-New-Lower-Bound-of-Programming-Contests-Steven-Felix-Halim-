bcb89fb4e59bd368003c288b1051effb687469b7
khalilalquraan <36332884+khalilalquraan@users.noreply.github.com>
2019-08-12 19:16:05 +0300

    Add files via upload

---
+++ Introduction/Getting Started: The Easy Problems/Super Easy/12577 - Hajj-e-Akbar.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(int argc, char const *argv[]) {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    string x;
+    int C = 1;
+    while(cin >> x) {
+        if(x == "*")break;
+        if(x == "Hajj")
+            printf("Case %d: Hajj-e-Akbar\n", C++ );
+        else
+            printf("Case %d: Hajj-e-Asghar\n", C++ );
+    }
+}
