79d8c2a23414d1278bd3ed53360c698665f931d5
khalilalquraan <36332884+khalilalquraan@users.noreply.github.com>
2019-08-23 04:56:15 +0300

    Add files via upload

---
+++ Introduction/Getting Started: The Easy Problems/Medium/12478 - Hardest Problem Ever (Easy).cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define ld long double
+using namespace std;
+int main(int argc, char const *argv[]) {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    cout << "KABIR" << endl;
+}
