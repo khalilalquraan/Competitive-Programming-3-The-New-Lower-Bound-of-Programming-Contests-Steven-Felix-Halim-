0dc31b1c3c380f3472ba6841dc25f35c4b307787
khalilalquraan <36332884+khalilalquraan@users.noreply.github.com>
2019-09-05 02:37:37 +0300

    Add files via upload

---
+++ 11173 - Grey Codes.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define MOD 998244353
+using namespace std;
+int main(int argc, char const *argv[]) {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t;
+    cin >> t;
+    while(t--) {
+        ll n, k;
+        cin >> n >> k;
+        long long ans;
+        cout << (k ^ (k >> 1)) << endl;
+    }
+}
